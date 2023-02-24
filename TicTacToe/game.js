b65b1ee6884758fc8f590d3f743fa266cc120619
Shane24335 <Shane24335@live.ytech.edu>
2023-02-24 10:10:51 -0500

    created ai function and buttons

--- TicTacToe/game.js
+++ TicTacToe/game.js
@@ -3,3 +3,16 @@ function restartGame() {
     location.reload();
 }
 
+function multi() {
+
+}
+
+function single() {
+    const ai_player = 'x';
+    function emptySquares () {
+        return origBoard.filter(item => typeof item === 'number');
+    };
+    function botSpot () {
+        return emptySquares()[0];
+    }
+}
