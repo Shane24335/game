9a94955b6ace5b05f22db9a72aa8ec453d7050ca
Shane24335 <Shane24335@live.ytech.edu>
2023-02-24 10:28:10 -0500

    Update game.js

--- TicTacToe/game.js
+++ TicTacToe/game.js
@@ -2,17 +2,3 @@
 function restartGame() {
     location.reload();
 }
-
-function multi() {
-
-}
-
-function single() {
-    const ai_player = 'x';
-    function emptySquares () {
-        return origBoard.filter(item => typeof item === 'number');
-    };
-    function botSpot () {
-        return emptySquares()[0];
-    }
-}
