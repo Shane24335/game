2958d5adc1cd5d874a1ec5a97e092ff0017e1f8d
Shane24335 <Shane24335@live.ytech.edu>
2023-02-28 09:58:56 -0500

    added ai moves and ids for cells

--- TicTacToe/game.js
+++ TicTacToe/game.js
@@ -5,7 +5,21 @@ const statusDisplay = document.querySelector('.game-status');
 let gameActive = true;
 
 // Store current player
-let currentPlayer = "X";
+const playerCreator = {
+    names: [
+        playerOneName = document.getElementById("player1name"),
+        playerTwoName = document.getElementById("player2name")
+    ],
+    scores: [
+        playerOneScore = document.getElementById("playeronescore"),
+        playerTwoScore = document.getElementById("playertwoscore")
+    ],
+    createPlayer(name) {
+        return {
+            name: name,
+        }
+    },
+};
 
 // Store game state in empty strings in the form of an array
 let gameState = ["", "", "", "", "", "", "", "", ""];
@@ -22,14 +36,63 @@ const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 statusDisplay.innerHTML = currentPlayerTurn();
 
 // Actual game functions
-function cellPlayed(clickedCell, clickedCellIndex) {
+const rules = {
+    cellPlayed(clickedCell, clickedCellIndex) {
+        gameState[clickedCellIndex] = currentPlayer;
+        clickedCell.innerHTML = currentPlayer;
+    },
+    resultPopup(result, player) {
+        let resultDivOverlay = document.createElement("div")
+        resultDivOverlay.id = "resultdivoverlay"
+        document.body.appendChild(resultDivOverlay)
+        let resultDiv = document.createElement("div");
+        resultDiv.id = "resultdiv"
+        resultDivOverlay.appendChild(resultDiv)
+        let resultDivTitleBar = document.createElement("div")
+        resultDivTitleBar.id = "resultdivtitlebar"
+        resultDiv.appendChild(resultDivTitleBar)
+        let resultDivTitle = document.createElement("span")
+        resultDivTitle.id = "resultdivtitle"
+        resultDivTitle.innerHTML = "RESULT"
+        resultDivTitleBar.appendChild(resultDivTitle)
+        let resultDivContent = document.createElement("div")
+        resultDivContent.id = "resultdivcontent"
+        resultDivContent.innerHTML = ""
+        resultDiv.appendChild(resultDivContent)
+        let resultButton = document.createElement("button")
+        resultButton.id = "resultbutton"
+        resultButton.type = "button"
+        resultButton.innerHTML = "OK"
+        resultDiv.appendChild(resultButton)
+        resultButton.addEventListener("click", () => {
+            document.body.removeChild(resultDivOverlay)
+            restartGame()
+        });
+        if (result === "win") {
+            resultDivContent.innerHTML = player + " WINS"
+        } else if (result === "draw") {
+            resultDivContent.innerHTML = "DRAW"
+        };
+    }
+}
 
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
+const gameBoard = {
+    board: document.getElementsByClassName("cell"),
+    boxes: [
+        cell0 = document.getElementById("cell0"),
+        cell1 = document.getElementById("cell1"),
+        cell2 = document.getElementById("cell2"),
+        cell3 = document.getElementById("cell3"),
+        cell4 = document.getElementById("cell4"),
+        cell5 = document.getElementById("cell5"),
+        cell6 = document.getElementById("cell6"),
+        cell7 = document.getElementById("cell7"),
+        cell8 = document.getElementById("cell8"),
+    ]
 }
 
 // 8 possible winning conditions
-const winningConditions = 
+const winningConditions =
     [
         [0, 1, 2],
         [0, 3, 6],
@@ -103,3 +166,33 @@ function restartGame() {
     location.reload();
 }
 
+function cpuMoves() {
+    function computerPlay() {
+        let random = [
+            gameBoard.cell[0], gameBoard.cell[1], gameBoard.cell[2],
+            gameBoard.cell[3], gameBoard.cell[4], gameBoard.cell[5],
+            gameBoard.cell[6], gameBoard.cell[7], gameBoard.cell[8]
+        ];
+        let randomBox = random[Math.random() * random.length];
+        if (
+            gameBoard.cell[0].innerHTML !== "" && gameBoard.cell[0].innerHTML !== "" && gameBoard.cell[0].innerHTML !== "" &&
+            gameBoard.cell[0].innerHTML !== "" && gameBoard.cell[0].innerHTML !== "" && gameBoard.cell[0].innerHTML !== "" &&
+            gameBoard.cell[0].innerHTML !== "" && gameBoard.cell[0].innerHTML !== "" && gameBoard.cell[0].innerHTML !== "") {
+            return
+        } else if (randomBox.innerHTML == "X" || randomBox.innerHTML == "0") {
+            computerPlay()
+        } else if (randomBox.innerHTML == "") {
+            randomBox.innerHTML = "O"
+        }
+    }
+    for (let i = 0; i < gameBoard.board.length; i++) {
+        gameBoard.board[i].addEventListener("click", function move2() {
+            if(gameBoard.board[i].innerHTML == "") {
+                gameBoard.board[i].innerHTML= "X"
+                computerPlay()
+            } else return
+
+        })
+        
+    }
+}
